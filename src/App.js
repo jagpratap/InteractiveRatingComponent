8a08ca913c03ee9d7e0ba4006b36858dd2c9855b
jagpratap <jagpratapchauhan@gmail.com>
2022-09-02 12:06:16 +0530

    Refactored boilerPlate+eslint&sass integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { StrictMode } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./global.scss";
+
+const App = () => (
+  <StrictMode>
+    App
+  </StrictMode>
+);
 
 export default App;
